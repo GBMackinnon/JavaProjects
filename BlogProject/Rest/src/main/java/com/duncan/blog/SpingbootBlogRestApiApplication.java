27c602c4039a98aa644f1d804ec4f70851bd4ba2
Duncan <MacKinnon>
2023-02-23 11:40:13 +0000

    Added JPA project. Added as a dependency to Rest and Root

---
+++ BlogProject/Rest/src/main/java/com/duncan/blog/SpingbootBlogRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.duncan.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingbootBlogRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingbootBlogRestApiApplication.class, args);
+	}
+
+}
